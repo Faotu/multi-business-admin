3cf91c42ff5a25922ccaf08d94662ac59935be17
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-01-13 18:24:00 +0100

    form and input manipulation

--- components/modals/storeModal.tsx
+++ components/modals/storeModal.tsx
@@ -2,11 +2,29 @@
 
 import { useStoreModal } from "@/hooks/userModal";
 import { Modal } from "@/components/ui/modal";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
-  // To control the store
-
   const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Create a store code will be here
+  };
+
   return (
     <Modal
       title="Create a Store"
@@ -14,7 +32,28 @@ export const StoreModal = () => {
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
     >
-      Create New store form
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>
+                      Name
+                      <FormControl>
+                        <Input placeholder="multi-business" {...field} />
+                      </FormControl>
+                    </FormLabel>
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </div>
+      </div>
     </Modal>
   );
 };
