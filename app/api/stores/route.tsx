31496dad9587c0e7059aa75bace3f1dc1c8778db
Faotu Happy <79374710+Faotu@users.noreply.github.com>
2024-01-17 16:12:24 +0100

    Prisma and planet scale db connection

---
+++ app/api/stores/route.tsx
@@ -0,0 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unathorized user", { status: 401 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+  } catch (error) {
+    console.log("[STORE_POST]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
